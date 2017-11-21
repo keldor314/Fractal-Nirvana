8995533bc7d6af1ddf6e08ead810c7e870bc37ba
Steven Brodhead <Steven Brodhead>
2017-11-21 15:22:28 -0800

    Add project files.

---
+++ Fractal Nirvana GTK/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps.GTK;
+using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms.Platform.GTK.Helpers;
+
+
+
+namespace Fractal_Nirvana.GTK
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+            FormsMaps.Init(string.Empty);
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Fractal Nirvana");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
