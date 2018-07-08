e1ff6d5d31ddd40d3ec1be1a866be4b7be32acfa
Steven Brodhead <keldor314@gmail.com>
2018-07-08 11:24:50 -0600

    Work with preview updating and marshaling.

--- Fractal_Nirvana_Avalonia/Preview.cs
+++ Fractal_Nirvana_Avalonia/Preview.cs
@@ -14,6 +14,7 @@ namespace Fractal_Nirvana
         private Renderer renderer;
         private PrecisionTimer timer;
         private WritableBitmap target;
+        private bool layoutInitialized = false;
         public Preview ()
         {
             InitializeComponent();
@@ -31,22 +32,31 @@ namespace Fractal_Nirvana
             var size = Bounds.Size;
             var width = (int)size.Width;
             var height =(int)size.Height;
-            target = new WritableBitmap(width, height, PixelFormat.Rgba8888);
-            var buffer = target.Lock();
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    int c = (x + 1) & (y + 1);
-                    int color = (int)(0xff000000+(0x00ffffff&(c*40+c*700+c*100000)));
-                    Marshal.WriteInt32(buffer.Address, (x + y * width)*sizeof(int), color);
-                }
-            }
-            Source = target;
             renderer.StartRender(width, height);
+            layoutInitialized = true;
         }
         private void Update ()
         {
+            if (layoutInitialized)
+            {
+                var size = Bounds.Size;
+                var width = (int)size.Width;
+                var height = (int)size.Height;
+                target = new WritableBitmap(width, height, PixelFormat.Rgba8888);
+                var buffer = target.Lock();
+                var address = buffer.Address;
+                var time = System.DateTime.Now.Ticks;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int c = (int)((((x + 1L) & (y + 1L)) *time)/50000000L);
+                        int color = (int)(0xff000000 + (0x00ffffff & (c * 40 + c * 700 + c * 100000)));
+                        Marshal.WriteInt32(address, (x + y * width) * sizeof(int), color);
+                    }
+                }
+                Source = target;
+            }
             Task.Run(() =>
             {
                 timer.Await();
